01c3a0f7289796b4d8c6991f02b7c05b9ba639c7
Saviour Baakum <sbaakum@st.ug.edu.gh>
2025-08-06 18:59:42 +0000

    add inheritance and method overriding

---
+++ Inheritance and Mettod Overriding/Program.cs
@@ -0,0 +1,41 @@
+﻿// Animal.cs
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Dog.cs
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Cat.cs
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+class program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.MakeSound();
+        myDog.MakeSound();
+        myCat.MakeSound();
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
